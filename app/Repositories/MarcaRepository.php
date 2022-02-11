8a06fdde3b17120a0bd3fa2d48a338abeadd7fc0
WillianyV <41123970+WillianyV@users.noreply.github.com>
2022-02-11 17:24:09 -0300

    paginacao-marcas

--- app/Repositories/MarcaRepository.php
+++ app/Repositories/MarcaRepository.php
@@ -7,5 +7,10 @@ namespace App\Repositories;
 class MarcaRepository extends AbstractRepository
 {
 
+    public function getResultadoPaginado($numero_de_registro_por_paginas)
+    {
+        return $this->model->paginate($numero_de_registro_por_paginas);
+    }
+
 }
 ?>
